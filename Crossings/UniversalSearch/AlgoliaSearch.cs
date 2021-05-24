bb6bb66ff686729e18bbe4c3bc9fe78827605220
Cameron Llewellyn <cameron.b.llewellyn@gmail.com>
2021-05-24 15:55:51 -0500

    Seach indexing working.

--- Crossings/UniversalSearch/AlgoliaSearch.cs
+++ Crossings/UniversalSearch/AlgoliaSearch.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
-using System.Reflection;
 
-using Nest;
 
-using Newtonsoft.Json.Linq;
 
 using Rock.Attribute;
 using Rock.Data;
@@ -18,6 +14,7 @@ using Rock.Web.Cache;
 using Rock.UniversalSearch;
 using Algolia.Search;
 using Algolia.Search.Clients;
+using Algolia.Search.Models.Search;
 
 namespace Crossings.UniversalSearch
 {
@@ -152,6 +149,24 @@ namespace Crossings.UniversalSearch
         }
 
         /// <summary>
+        /// Searches the specified query.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="searchType">Type of the search.</param>
+        /// <param name="entities">The entities.</param>
+        /// <param name="fieldCriteria">The field criteria.</param>
+        /// <param name="size">The size.</param>
+        /// <param name="from">From.</param>
+        /// <param name="totalResultsAvailable">The total results available.</param>
+        /// <returns></returns>
+        public override List<IndexModelBase> Search(string query, SearchType searchType, List<int> entities, SearchFieldCriteria fieldCriteria, int? size, int? from, out long totalResultsAvailable)
+        {
+            List<IndexModelBase> documents = null;
+            totalResultsAvailable = 0;
+            return documents;
+        }
+
+        /// <summary>
         /// Deletes the type of the documents by.
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -165,8 +180,145 @@ namespace Crossings.UniversalSearch
 
 
             SearchIndex index = _client.InitIndex(indexName);
-            index.DeleteByAsync
-            _client.DeleteByQueryAsync<T>( indexName, typeof( T ).Name.ToLower(), d => d.MatchAll() );
+            index.ClearObjectsAsync();
+            //EClient.DeleteByQueryAsync<T>( indexName, typeof( T ).Name.ToLower(), d => d.MatchAll() );
+        }
+
+        /// <summary>
+        /// Deletes the document.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="document">The document.</param>
+        /// <param name="indexName">Name of the index.</param>
+        public override void DeleteDocument<T>(T document, string indexName = null)
+        {
+            if (indexName == null)
+            {
+                indexName = document.GetType().Name.ToLower();
+            }
+            SearchIndex index = _client.InitIndex(indexName);
+            var query = new Query { Filters = $"id:{document}" };
+            //EClient.Delete<T>(document, d => d.Index(indexName));
+        }
+
+        /// <summary>
+        /// Creates the index.
+        /// </summary>
+        /// <param name="documentType">Type of the document.</param>
+        /// <param name="deleteIfExists">if set to <c>true</c> [delete if exists].</param>
+        public override void CreateIndex(Type documentType, bool deleteIfExists = true)
+        {
+            var indexName = documentType.Name.ToLower();
+
+            object instance = Activator.CreateInstance(documentType);
+
+            // check if index already exists
+            SearchIndex index = _client.InitIndex(indexName);
+            var existsResponse = index.Exists();
+
+            if (existsResponse)
+            {
+                // If Index exists check value and possibly delete.
+                if (deleteIfExists)
+                {
+                    //Algolia can clear the index (this could get 
+                    this.DeleteIndex(documentType);
+                }
+                else
+                {
+                    return;
+                }
+            }
+            // For Algolia the model does not matter the documents do not need to be formatted.
+
+        }
+
+        /// <summary>
+        /// Deletes the index.
+        /// </summary>
+        /// <param name="documentType">Type of the document.</param>
+        public override void DeleteIndex(Type documentType)
+        {
+            var indexName = documentType.Name.ToLower();
+            SearchIndex index = _client.InitIndex(indexName);
+            index.ClearObjectsAsync();
+        }
+
+        /// <summary>
+        /// Searches the specified query.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="searchType">Type of the search.</param>
+        /// <param name="entities">The entities.</param>
+        /// <param name="fieldCriteria">The field criteria.</param>
+        /// <param name="size">The size.</param>
+        /// <param name="from">From.</param>
+        /// <returns></returns>
+        public override List<IndexModelBase> Search(string query, SearchType searchType = SearchType.Wildcard, List<int> entities = null, SearchFieldCriteria fieldCriteria = null, int? size = null, int? from = null)
+        {
+            long totalResultsAvailable = 0;
+            return Search(query, searchType, entities, fieldCriteria, size, from, out totalResultsAvailable);
+        }
+
+
+
+        /// <summary>
+        /// Deletes the document by property.
+        /// </summary>
+        /// <param name="documentType">Type of the document.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="propertyValue">The property value.</param>
+        public override void DeleteDocumentByProperty(Type documentType, string propertyName, object propertyValue)
+        {
+            string jsonSearch = string.Format(@"{{
+""term"": {{
+      ""{0}"": {{
+                ""value"": ""{1}""
+      }}
+        }}
+}}", Char.ToLowerInvariant(propertyName[0]) + propertyName.Substring(1), propertyValue);
+
+            var response = true;
+        }
+
+        /// <summary>
+        /// Deletes the document by identifier.
+        /// </summary>
+        /// <param name="documentType">Type of the document.</param>
+        /// <param name="id">The identifier.</param>
+        public override void DeleteDocumentById(Type documentType, int id)
+        {
+            this.DeleteDocumentByProperty(documentType, "id", id);
+        }
+
+        /// <summary>
+        /// Gets the document by identifier.
+        /// </summary>
+        /// <param name="documentType">Type of the document.</param>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public override IndexModelBase GetDocumentById(Type documentType, int id)
+        {
+            return GetDocumentById(documentType, id.ToString());
+        }
+
+        /// <summary>
+        /// Gets the document by identifier.
+        /// </summary>
+        /// <param name="documentType">Type of the document.</param>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public override IndexModelBase GetDocumentById(Type documentType, string id)
+        {
+            var indexName = documentType.Name.ToLower();
+
+            //var request = new GetRequest(indexName, indexName, id) { };
+
+            var result = true;
+
+            IndexModelBase document = new IndexModelBase();
+
+            return document;
         }
 
     }
