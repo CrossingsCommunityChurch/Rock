38d6c970b188be31b00cd02c5519606060541f8d
Cameron <cameron.b.llewellyn@gmail.com>
2021-10-14 08:25:41 -0500

    Add crossings project

---
+++ Crossings/UniversalSearch/CccSitePageIndex.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rock.Model;
+using Rock.UniversalSearch.IndexModels;
+using Rock.UniversalSearch.IndexModels.Attributes;
+
+namespace Crossings.UniversalSearch
+{
+    class CccSitePageIndex : IndexModelBase
+    {
+        /// <summary>
+        /// Gets or sets the site identifier.
+        /// </summary>
+        /// <value>
+        /// The site identifier.
+        /// </value>
+        [RockIndexField]
+        public int SiteId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the site.
+        /// </summary>
+        /// <value>
+        /// The name of the site.
+        /// </value>
+        [RockIndexField]
+        public string SiteName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page title.
+        /// </summary>
+        /// <value>
+        /// The page title.
+        /// </value>
+        [RockIndexField(Boost = 4)]
+        public string PageTitle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page summary.
+        /// </summary>
+        /// <value>
+        /// The page summary.
+        /// </value>
+        [RockIndexField(Boost = 2)]
+        public string PageSummary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page keywords.
+        /// </summary>
+        /// <value>
+        /// The page keywords.
+        /// </value>
+        [RockIndexField(Boost = 8)]
+        public string PageKeywords { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
+        /// <value>
+        /// The URL.
+        /// </value>
+        [RockIndexField(Index = IndexType.NotIndexed)]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content.
+        /// </summary>
+        /// <value>
+        /// The content.
+        /// </value>
+        [RockIndexField]
+        public string Content { get; set; }
+
+        /// <summary>
+        /// gets or sets the tags. 
+        /// </summary>
+        public string Tags { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last indexed date time.
+        /// </summary>
+        /// <value>
+        /// The last indexed date time.
+        /// </value>
+        [RockIndexField(Type = IndexFieldType.Date)]
+        public DateTime LastIndexedDateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the icon CSS class.
+        /// </summary>
+        /// <value>
+        /// The icon CSS class.
+        /// </value>
+        [RockIndexField(Index = IndexType.NotIndexed)]
+        public override string IconCssClass
+        {
+            get
+            {
+                return "fa fa-desktop";
+            }
+        }
+
+        /// <summary>
+        /// Loads the by model.
+        /// </summary>
+        /// <param name="site">The site.</param>
+        /// <returns></returns>
+        public static SitePageIndex LoadByModel(Site site)
+        {
+            // site page does not have a Rock model
+            return null;
+        }
+    }
+}
---
+++ Crossings/Worflow/Action/FilterGroupByAttribute.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Web.Cache;
+using Rock.Workflow;
+using Rock.Workflow.Action.CheckIn;
+
+namespace Crossings.Worflow.Action
+{
+        /// <summary>
+        /// Removes (or excludes) the groups for each selected family member that are not specific to their age or birthdate.
+        /// </summary>
+        [ActionCategory("Check-In")]
+        [Description("Removes (or excludes) the groups for each selected family member that are not specific to their Attribute.")]
+        [Export(typeof(ActionComponent))]
+        [ExportMetadata("ComponentName", "Filter Groups By Attribute")]
+
+        [BooleanField("Remove", "Select 'Yes' if groups should be removed.  Select 'No' if they should just be marked as excluded.", true, "", 0)]
+        [AttributeField(Rock.SystemGuid.EntityType.GROUP, "Group Room Attribute", "Select the attribute used to define the Room Attributes of the group", true, false, order: 1)]
+    public class FilterGroupsByAttribute : CheckInActionComponent
+        {
+            /// <summary>
+            /// Executes the specified workflow.
+            /// </summary>
+            /// <param name="rockContext">The rock context.</param>
+            /// <param name="action">The workflow action.</param>
+            /// <param name="entity">The entity.</param>
+            /// <param name="errorMessages">The error messages.</param>
+            /// <returns></returns>
+            /// <exception cref="System.NotImplementedException"></exception>
+            public override bool Execute(RockContext rockContext, Rock.Model.WorkflowAction action, Object entity, out List<string> errorMessages)
+            {
+                var checkInState = GetCheckInState(entity, out errorMessages);
+                if (checkInState == null)
+                {
+                    return false;
+                }
+
+                var family = checkInState.CheckIn.CurrentFamily;
+                if (family != null)
+                {
+                var remove = GetAttributeValue(action, "Remove").AsBoolean();
+
+                    // get the Room Attribute
+                    var roomAttributeKey = string.Empty;
+                    var roomAttributeGuid = GetAttributeValue(action, "GroupRoomAttribute").AsGuidOrNull();
+                    if (roomAttributeGuid.HasValue)
+                    {
+                        var attribute = AttributeCache.Get(roomAttributeGuid.Value, rockContext);
+                        if (attribute != null)
+                        {
+                            roomAttributeKey = attribute.Key;
+                        }
+                    }
+
+                    foreach (var person in family.People)
+                    {
+                        person.Person.LoadAttributes();
+                        // Gets the guid of the attribute as a string.
+                        var personRoomAtt = person.Person.GetAttributeValue("RoomAttribute");
+
+                        foreach (var groupType in person.GroupTypes.ToList())
+                        {
+                            foreach (var group in groupType.Groups.ToList())
+                            {
+                                // First we check that the Attribute is not empty and it matches the group attribute.
+                                if (!string.IsNullOrWhiteSpace(roomAttributeKey))
+                                {
+                                    var groupRoomAtt = group.Group.GetAttributeValue(roomAttributeKey).ToStringSafe();
+                                if (string.IsNullOrWhiteSpace(groupRoomAtt) || personRoomAtt == groupRoomAtt) continue;
+                                    if (remove)
+                                    {
+                                        groupType.Groups.Remove(group);
+                                    }
+                                    else
+                                    {
+                                        group.ExcludedByFilter = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
